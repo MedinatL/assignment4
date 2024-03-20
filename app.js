5e966290f877dc541651f6e617e3207d00acb214
MedinatL <olamidemedinat@gmail.com>
2024-03-20 09:28:00 -0700

    update

--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-const express = require("express");
 
-const app = express();
+
+const express = require("express");
+const bodyParser = require('body-parser');
+
+const authorRoute = require('./routes/authors');
+
+
 PORT = 3000;
+const app = express();
+
+app.use(express.static('public'));
+              
+app.use('/authors', authorRoute);
+
 
 app.get("/", (req, res) => {
     res.status(200)
-    res.send("hello world");
+    res.send("get successful");
+})
+
+app.post("/", (req, res) => {
+    res.status(200)
+    res.send("post successful")
 })
 
 app.listen(PORT, () => {
     console.log(`server is running at port ${PORT}`);
-})
+});
 
